40162eecac080fd8f398a8740d2f1306af53079d
oidagsdhsagdhas44 <tornike.buchukuri@gau.edu.ge>
2023-02-22 01:38:03 +0400

    module4 task3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (!(character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z')) System.out.println("English");
+        else System.out.println("Non English");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        if ((symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z')){
+            if (symbol == 'A' || symbol == 'a' ||
+                    symbol == 'E' || symbol == 'e' ||
+                    symbol == 'I' || symbol == 'i' ||
+                    symbol == 'O' || symbol == 'o' ||
+                    symbol == 'U' || symbol == 'u') System.out.println("Vowel");
+            else System.out.println("Consonant");
+        }
+        else System.out.println("wrong alphabet!");
     }
 }
